0a7347dd26e9b3e76eabbc760262466125206367
dimonk1ss <dklrrb@gmail.com>
2022-09-12 13:36:02 +0300

    Add Prototype

---
+++ 1.5Prototype/1.Prototype/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._5Prototype
+{
+
+    public class Person
+    {
+        
+        public IdInfo Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sername { get; set; } 
+        public string Diagnoz { get; set; }
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+           
+            clone.Id = new IdInfo(Id.IdNumber);
+            clone.Name = String.Copy(Name);
+
+            return clone;
+        }
+    }
+}
---
+++ 1.5Prototype/1.Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _1._5Prototype
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+            Person p1 = new Person();
+            p1.Age = 18;
+            p1.Name = "Nicolas";
+            p1.Sername = "Keij";
+            p1.Diagnoz = "Cancer";
+            p1.Id = new IdInfo(666);
+            Person p2 = p1.ShallowCopy();
+            
+            Person p3 = p1.DeepCopy();
+
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+        }
+
+        public static void DisplayValues(Person p)
+        {
+            Console.WriteLine("Name: " + p.Name);
+            Console.WriteLine("Sername: " + p.Sername);
+            Console.WriteLine("Diagnoz: " + p.Diagnoz);
+            Console.WriteLine("Age: " + p.Age);
+            Console.WriteLine("Id: " + p.Id.IdNumber);
+        }
+    }
+}
+
